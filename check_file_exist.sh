b6853c94681dba6ea6465439f74c70d4527c70bc
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-10 22:36:01 +0100

    commit

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 #Demande à l'utilisateur d'entrer un nom de fichier
 #echo "Entrez le nom du fichier :" 
- read -p "Entrez le nom du fichier :  " 'filename'
+ read -p "Entrez le nom du fichier : " 'filename'
 # Vérifie si le fichier existe et est un fichier régulier
 if [ -f "$filename" ]; then 
   echo "Le fichier '$filename' existe."
