88b525499d750551d68fe555c209f7a13a1042e0
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-11 00:35:18 +0100

    commit

--- list_files.sh
+++ list_files.sh
@@ -2,5 +2,5 @@
 # Cette commande affiche tous les fichiers du répertoire courant
 
  ls -la  
-# -a affiche tous les fichiers, y compris ceux cachés (les fichiers commençant par un .
-#ffiche des informations détaillées
+# -a affiche tous les fichiers, y compris ceux cachés (les fichiers commençant par un
+# affiche des informations détaillées
