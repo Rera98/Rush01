e81f9e7e5f78853279d448c218bdbe371d2cc0d2
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-10 23:21:29 +0100

    commit

--- create_directory.sh
+++ create_directory.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # Ce script crée un répertoire nommé test_directory avec vérification
 
-mkdir [ -p] test_directory
+mkdir -p test_directory
+#Crée tous les répertoires parents nécessaires s’ils n’existent pas.
+
+# -p Évite l’erreur si le répertoire existe déjà.
+
