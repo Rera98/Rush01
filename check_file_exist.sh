204fa57ebbfe03f55963ddd5e5d0c02bacd70563
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-11 02:59:13 +0100

    commit

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,11 +1,11 @@
-#!/bin/bash 
-#Demande à l'utilisateur d'entrer un nom de fichier  
- echo
- read -p  "Entrez le nom du fichier : " filename
- 
-# Vérifie si le fichier existe et est un fichier régulier
-if [ -f "$filename" ]; then  
-  echo "Le fichier '$filename' existe." 
-else 
-  echo "Le fichier '$filename' n'existe pas."  
-fi
+#!/bin/bash
+#demande à l'utilisateur de rentrer un nom de fichier
+read -p "Entrez le nom du fichier : " filename
+
+[ -f "$filename" ] && echo "Le fichier '$filename' existe." || echo "Le fichier '$filename' n'existe pas."
+
+# [ -f "$filename" ] : teste si c’est un fichier régulier.
+
+# && : exécute la commande suivante si le test est vrai.
+
+# || : exécute la commande suivante si le test est faux.
